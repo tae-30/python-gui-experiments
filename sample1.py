6d9b02c4b0d3a820d98bc93040eb1af879880288
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2024-05-21 22:25:55 +0900

    refactor

--- sample1.py
+++ sample1.py
@@ -1,28 +1,32 @@
 import tkinter as tk
 
-
-# メッセージを表示する関数
-def display_message():
-    user_input = entry.get()
-    label.config(text=f"Hello, {user_input}!")
-
-
-# メインウィンドウの作成
+# ↓↓↓ お約束のコード ↓↓↓
 window = tk.Tk()
-window.title("Simple GUI App")
-window.geometry("300x200")
+window.title("GUI App")
+window.geometry("600x400")
+bg_color = "#333333"  # ダークグレー
+fg_color = "#FFFFFF"  # 白
+window.configure(bg=bg_color)
+# ↑↑↑ お約束のコード ↑↑↑
+
+
+def button_action():  # 関数の定義 ※ボタンが押されたときの動き
+    user_input = entry1.get()  # 入力値を取得
+    label1.config(text=f"Hello, {user_input}!")  # 画面に出力
+
 
 # 入力フィールドの作成
-entry = tk.Entry(window)
-entry.pack(pady=10)
+entry1 = tk.Entry(window, bg=fg_color, fg=bg_color)
+entry1.pack(pady=10)
 
 # ボタンの作成
-button = tk.Button(window, text="Submit", command=display_message)
-button.pack(pady=10)
+button1 = tk.Button(window, text="Submit", command=button_action)
+button1.pack(pady=10)
 
 # 出力ラベルの作成
-label = tk.Label(window, text="")
-label.pack(pady=10)
+label1 = tk.Label(window, text="", bg=bg_color, fg=fg_color)
+label1.pack(pady=10)
 
-# ウィンドウの表示とイベントループの開始
+# ↓↓↓ お約束のコード ↓↓↓
 window.mainloop()
+# ↑↑↑ お約束のコード ↑↑↑
