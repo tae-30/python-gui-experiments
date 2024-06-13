d3f88cb81dee102afd33d9fab2e3322d978a1e2b
tae-30 <yuut0302@icloud.com>
2024-06-13 12:06:46 +0900

    first commit

---
+++ practice.py
@@ -0,0 +1,21 @@
+players_img = [
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Empty.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Dragon.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Crystal.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Hero.png",
+    "https://paiza-webapp.s3.amazonaws.com/files/learning/rpg/Heroine.png",
+]
+
+team = []
+while True:
+    line = input()
+    if line == "_":
+        break
+    team.append(line.split(","))
+
+team = []
+while True:
+    line = input()
+    if line == "_":
+        break
+    team.append(line.split(","))
